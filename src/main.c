3fbe86d8bc22e4a5158c5fc33843bbe0c25fe81c
TayehTech <yacoub.altayeh@icloud.com>
2024-10-26 12:04:40 +0300

    -_-

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <libft.h>
 #include <push_swap.h>
+#include <sys/errno.h>
 
 int	togger_complement(int num)
 {
@@ -9,44 +10,9 @@ int	togger_complement(int num)
     return (num + 1);
 }
 
-t_list	*init_stack(const int argc, const char **argv)
-{
-	t_list	*node;
-	t_list	*stack;
-	int		number;
-	int		i;
-
-	stack = NULL;
-	i = 1;
-	while (i < argc)
-	{
-		number = ft_atoi(argv[i]);
-		//number = togger_complement(number);
-		if (ft_lstsearch(stack, (t_content)number))
-		{
-			ft_lstclear(&stack, NULL);
-			exit(1);
-		}
-		node = ft_lstnew((t_content)number);
-		if (!node)
-		{
-			ft_lstclear(&stack, NULL);
-			exit(1);
-		}
-		ft_lstpush_back(&stack, node);
-		i++;
-	}
-	return (stack);
-}
-
 void print_stack(t_content value)
 {
-	//int i32;
-
-	//i32 = togger_complement(value.i32);
-	
-
-	ft_fprintf(2, "%d, ", value.i32);
+	ft_fprintf(0, "%11d\n", value.i32);
 }
 
 void print_step(t_content content)
@@ -61,7 +27,65 @@ void print_step2(t_content content)
 	ft_fprintf(2, "%s ", step->title);
 }
 
-int	main(const int argc, const char **argv)
+int	add_new_node(t_list **lst, int number)
+{
+	t_list	*node;
+
+	ft_printf("'%d'\n", number);
+	ft_lstiter(*lst, print_stack);
+	if (ft_lstsearch(*lst, (t_content)number))
+	{
+		ft_lstclear(lst, NULL);
+		ft_printf("test\n");
+		return (EINVAL);
+	}
+	node = ft_lstnew((t_content)number);
+	if (!node)
+	{
+		ft_lstclear(lst, NULL);
+		return (ENOMEM);
+	}
+	ft_lstpush_back(lst, node);
+	return (0);
+}
+int	init_stack(t_list **stack, const int argc, char **argv)
+{
+	int			i;
+	char		*slice;
+	int			ret;
+
+	i = 1;
+	while (i < argc)
+	{
+		slice = ft_strtok(argv[i], ", \n\t");
+		while (slice)
+		{
+			if (!ft_strcheck(slice, ft_isdigit))
+				return (EINVAL);
+			ret = add_new_node(stack, ft_atoi(slice));
+			if (ret)
+				return (ret);
+			slice = ft_strtok(NULL, ", \n\t");
+		}
+		i++;
+	}
+	return (0);
+}
+
+void	free_lst(void **__unsafe_indexable lst)
+{
+	size_t	i;
+
+	i = 0;
+	while (lst[i])
+	{
+		free(lst[i]);
+		i++;
+	}
+	free(lst);	
+}
+
+int	main(const int argc, char **argv)
 {
 	t_list	*a;
 	t_list	*b;
@@ -71,7 +95,14 @@ int	main(const int argc, const char **argv)
 
 	if (argc == 1)
 		return (0);
-	a = init_stack(argc, argv);
+	a = NULL;
+	int err = init_stack(&a, argc, argv);
+	if (err)
+	{
+		errno = err;
+		perror("");
+		exit(1);
+	}
 	b = NULL;
 	steps = NULL;
 
@@ -89,10 +120,10 @@ int	main(const int argc, const char **argv)
 
 	ft_fprintf(2, "\n======== A ========\n");
 	ft_lstiter(a, print_stack);
-	ft_fprintf(2, "\n======== B ========\n");
-	ft_lstiter(b, print_stack);
+	// ft_fprintf(2, "\n======== B ========\n");
+	// ft_lstiter(b, print_stack);
 	//ft_fprintf(2, "\n----------------------------\n");
-	ft_fprintf(2, "total steps: %d\nsteps reducer: %d\n", count_after, count_before - count_after);
+	ft_fprintf(2, "total steps: %d\nsteps_reducer: %d\n", count_after, count_before - count_after);
 
 	ft_lstclear(&a, NULL);
 	ft_lstclear(&b, NULL);
--- src/sorters/radix_sort.c
+++ src/sorters/radix_sort.c
@@ -1,36 +1,61 @@
 #include "push_swap.h"
 
-static void	sort_by_bit(t_list **steps, t_list **a, t_list **b, int class)
+static int	sort_by_bit(t_list **steps, t_list **a, t_list **b, int class)
 {
 	int		mask;
+	size_t	size;
+	size_t	i;
+	int		ret;
 	//int		mask2;
 
 	mask = 1 << class;
+	size = ft_lstsize(*a);
+	i = 0;
 	// mask2 = 0;
 	// if (class <= 31)
 	// 	mask2 = 1 << (class + 1);
-
-
-	while(check_mask(*a, mask))
+	while (i < size)
 	{
 		if ((*a)->content.i32 & mask)
-		{
-			if (pb(steps, a, b) == 1)
-				(void)0;//ft_fprintf(2, "pb ");
-			// if ((*b)->content.i32 & mask2)
-			// 	RUN_OP(rb);
-		}
+			ret = pb(steps, a, b);
 		else
+			ret = ra(steps, a, b);
+		if (ret == -1)
 		{
-			if (ra(steps, a, b) == 1)
-				(void)0;//ft_fprintf(2, "ra ");
+			ft_lstclear(steps, (void *)free);
+			return (-1);
+		}
+		i++;
+	}
+	while (*b)
+	{
+		if (pa(steps, a, b) == -1)
+		{
+			ft_lstclear(steps, (void *)free);
+			return (-1);
 		}
 	}
-	while (*b)//  && !((*b)->content.i32 & mask2))
-	{ 
-		if (pa(steps, a, b) == 1)
-			(void)0;//ft_fprintf(2, "pa ");
-	}
+	return (1);
+	// while(check_mask(*a, mask))
+	// {
+	// 	if ((*a)->content.i32 & mask)
+	// 	{
+	// 		if (pb(steps, a, b) == 1)
+	// 			(void)0;//ft_fprintf(2, "pb ");
+	// 		// if ((*b)->content.i32 & mask2)
+	// 		// 	RUN_OP(rb);
+	// 	}
+	// 	else
+	// 	{
+	// 		if (ra(steps, a, b) == 1)
+	// 			(void)0;//ft_fprintf(2, "ra ");
+	// 	}
+	// }
+	// while (*b)//  && !((*b)->content.i32 & mask2))
+	// { 
+	// 	if (pa(steps, a, b) == 1)
+	// 		(void)0;//ft_fprintf(2, "pa ");
+	// }
 }
 
 t_list *radix_sort(t_list **a, t_list **b)
@@ -47,7 +72,8 @@ t_list *radix_sort(t_list **a, t_list **b)
 	return (steps);
 	while (value)
 	{
-		sort_by_bit(&steps, a, b, class++);
+		if (check_mask(*a, 1 << class))
+			sort_by_bit(&steps, a, b, class++);
 		value >>= 1;
 	}
 	return (steps);
