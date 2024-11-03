a34a46603088f4cfdd1451e0f1821a92a67f0e72
Yacoub Altayeh (Home) <yacoub.altayeh@icloud.com>
2024-11-03 08:33:42 +0300

    merge

--- src/merge_2_b.c
+++ src/merge_2_b.c
@@ -1,6 +1,6 @@
 #include "push_swap.h"
 
-static int	block_a(t_stack *a, t_node **cur)
+static int	_block_a(t_stack *a, t_node **cur)
 {
 	t_node	*_cur;
 	int		res;
@@ -20,7 +20,7 @@ static int	block_a(t_stack *a, t_node **cur)
 	return (0);
 }
 
-static int	block_b(t_stack *b, t_node **cur)
+static int	_block_b(t_stack *b, t_node **cur)
 {
 	t_node	*_cur;
 	int		res;
@@ -40,15 +40,15 @@ static int	block_b(t_stack *b, t_node **cur)
 	return (0);
 }
 
-int merge_2_b(t_stack *steps, t_stack *a, t_stack *b)
+int _merge_2_b(t_stack *steps, t_stack *a, t_stack *b)
 {
 	t_node	*cur_a;
 	t_node	*cur_b;
 	int		res;
 
-	if (block_a(a, &cur_a))
+	if (_block_a(a, &cur_a))
 		return (-1);
-	if (block_b(b, &cur_b))
+	if (_block_b(b, &cur_b))
 		return (-1);
 
 
@@ -84,5 +84,88 @@ int merge_2_b(t_stack *steps, t_stack *a, t_stack *b)
 				return (-1);
 		}
 	}
+	return (0);
+}
+
+
+
+static int	block_a(t_stack *a, int *count)
+{
+	t_node	*cur;
+	int		test;
+
+
+	*count = 0;
+	cur = a->head;
+	while (cur)
+	{
+		if (cur->next)
+			if (ft_cmp_node(cur, cur->next, &test))
+				return (-1);
+		cur = cur->next;
+		(*count)++;
+		if (test >= 0)
+			break ;
+	}
+	return (0);
+}
+
+static int	block_b(t_stack *b, int *count)
+{
+	t_node	*cur;
+	int		test;
+
+	cur = b->tail;
+	*count = 0;
+	while (cur)
+	{
+		if (cur->prev)
+			if (ft_cmp_node(cur, cur->prev, &test))
+				return (-1);
+		cur = cur->prev;
+		(*count)++;
+		if (test >= 0)
+			break ;
+	}
+	return (0);
+}
+
+int merge_2_b(t_stack *steps, t_stack *a, t_stack *b)
+{
+	int	count_a;
+	int	count_b;
+	int		res;
+
+	if (block_a(a, &count_a))
+		return (-1);
+	if (block_b(b, &count_b))
+		return (-1);
+
+	ft_printf("a: %d,  b: %d\n", count_a, count_b);
+
+	while (count_b && count_a)
+	{
+		if (ft_cmp_node(a->head, b->tail, &res))
+			return (-1);
+		if (res < 0)
+		{
+			if (pb(steps, a, b) == -1)
+				return (-1);
+			count_a--;
+		}
+		else
+		{
+			if (rrb(steps, a, b) == -1)
+				return (-1);
+			count_b--;
+		}
+	}
+	ft_printf("a: %d,  b: %d\n", count_a, count_b);
+	while (count_a--)
+	{
+		if (pb(steps, a, b) == -1)
+			return (-1);
+	}
+	
 	return (0);
 }
